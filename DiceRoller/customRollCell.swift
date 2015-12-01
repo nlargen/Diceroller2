9843f9bc161e473a490c1438b435957a658b876f
Nicholas Largen <nicholaslargen@nicholass-mbp-3.cuw.edu>
2015-11-30 22:59:56 -0600

    userpageload issue

---
+++ DiceRoller/customRollCell.swift
@@ -0,0 +1,24 @@
+//
+//  customRollCell.swift
+//  DiceRoller
+//
+//  Created by Nicholas Largen on 11/30/15.
+//  Copyright © 2015 awesomefat. All rights reserved.
+//
+
+import UIKit
+
+class customRollCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
